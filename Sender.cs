3ed7e7816661b722c12030d8a3b6a6b758fc3e5f
wjl0720 <18528300959@163.com>
2024-09-10 22:55:29 +0800

    Update Sender.cs

--- Sender.cs
+++ Sender.cs
@@ -7,32 +7,29 @@ public class Sender : MonoBehaviour
 
     public BulletObject bullet;
 
-    public float currentAngle = 0;//当前发射角度
-    public float currentAngularVelocity = 0;  //当前角速度
+    public float currentAngle = 0;
+    public float currentAngularVelocity = 0;  
    public  float currentTime = 0;
 
 
     public void Awake()
     {
-        currentAngle = bullet.InitRotation;//初始旋转
-        currentAngularVelocity=bullet.SenderAngularVelocity;//当前初始角速度
+        currentAngle = bullet.InitRotation;
+        currentAngularVelocity=bullet.SenderAngularVelocity;
 
     }
 
     private void FixedUpdate()
     {
-        //v=a*t
+        
         currentAngularVelocity =Mathf.Clamp(currentAngularVelocity+ bullet.SenderAcceleration * Time.fixedDeltaTime,
             -bullet.SenderMaxAngularVelocity,bullet.SenderMaxAngularVelocity);
-        //更新角度
         currentAngle += currentAngularVelocity * Time.fixedDeltaTime;
-        //限制角度
         if(Mathf.Abs(currentAngle)>720f)
         {
             currentAngle -= Mathf.Sign(currentAngle) * 360f;
 
         }
-        //更新时间
         currentTime += Time.fixedDeltaTime;
         if(currentTime>bullet.SenderInterval)
         {
@@ -46,7 +43,6 @@ public class Sender : MonoBehaviour
     private void SendByCount(int count,float angle)
     {
         float temp = count%2==0? angle+bullet.LineAngle/2 : angle;
-        //编译每一条线
         for(int i = 0; i < count; i++)
         {
             temp += Mathf.Pow(-1, 1) * i * bullet.LineAngle;
@@ -59,12 +55,10 @@ public class Sender : MonoBehaviour
         
     }
     private void Send (float angle)
-    {//生成子弹
+    {
         GameObject go = Instantiate(bullet.prefabs);
         go.transform.position = transform.position;
-        //设置子弹初始旋转
         go.transform.rotation = Quaternion.Euler(0, 0, angle);
-        //初始化子弹的配置信息
         var bh=go.AddComponent<BulletBehaviour>();
         
         InitBullet(bh);
